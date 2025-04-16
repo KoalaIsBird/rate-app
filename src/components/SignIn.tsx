7e54e3d9e63668e3a63180b95dc2afa58254c75f
André <koalaisbird@gmail.com>
2025-04-16 10:51:25 +0200

    part 10b finsished

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,7 +1,81 @@
-import { Text } from "./Text";
+import { useFormik } from 'formik';
+import { Text } from './Text';
+import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+import theme from '../theme';
+import * as yup from 'yup';
+
+
+
+
+const styles = StyleSheet.create({
+  formInput: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: 'theme.colors.textSecondary',
+    padding: 16
+  }
+});
+
+const formSchema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required')
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues: { username: '', password: '' },
+    onSubmit: values =>
+      console.log(`username: ${values.username}, password: ${values.password}`),
+    validationSchema: formSchema
+  });
+
+  return (
+    <View style={{ padding: 16, gap: 20, backgroundColor: 'white' }}>
+      <TextInput
+        style={[
+          styles.formInput,
+          formik.touched.username && formik.errors.username
+            ? { borderColor: theme.colors.error }
+            : { borderColor: theme.colors.textSecondary }
+        ]}
+        placeholder='Username'
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{ color: theme.colors.error }}>{formik.errors.username}</Text>
+      )}
+
+      <TextInput
+        secureTextEntry
+        style={[
+          styles.formInput,
+          formik.touched.password && formik.errors.password
+            ? { borderColor: theme.colors.error }
+            : { borderColor: theme.colors.textSecondary }
+        ]}
+        placeholder='Password'
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: theme.colors.error }}>{formik.errors.password}</Text>
+      )}
+
+      <Pressable
+        onPress={e => formik.handleSubmit()}
+        style={{
+          backgroundColor: theme.colors.primary,
+          justifyContent: 'center',
+          flexDirection: 'row',
+          padding: 16,
+          borderRadius: 4
+        }}
+      >
+        <Text style={{ color: 'white' }}>Sign In</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default SignIn;
--- src/theme.ts
+++ src/theme.ts
@@ -1,22 +1,34 @@
+import { Platform } from 'react-native';
+
 const theme = {
-    colors: {
-      textPrimary: '#24292e',
-      textSecondary: '#586069',
-      primary: '#0366d6',
-      headerColor: '#24292e',
-      appBackground: '#e1e4e8'
-    },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-    },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400' as '400',
-      bold: '700' as '700',
-    },
-  };
-  
-  export default theme;
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    headerColor: '#24292e',
+    appBackground: '#e1e4e8',
+    error: '#d73a4a'
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16
+  },
+  fonts: {
+    main: (() => {
+      switch (Platform.OS) {
+        case 'android':
+          return 'Roboto';
+        case 'ios':
+          return 'Arial';
+        default:
+          return 'System';
+      }
+    })()
+  },
+  fontWeights: {
+    normal: '400' as '400',
+    bold: '700' as '700'
+  }
+};
+
+export default theme;
